e0a513275676ae5c91b6ecd12a8b2ba3d931adc3
Sergey Royz <zjor.se@gmail.com>
2021-05-14 18:40:16 +0200

    load html from local file

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,26 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as util from 'util';
+
+const readFile = util.promisify(fs.readFile);
 
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Congratulations, your extension "serial-plotter" is now active!');
-
-	let disposable = vscode.commands.registerCommand('serial-plotter.run', () => {
-		const panel = vscode.window.createWebviewPanel(
-      'serialPlotter', 
-      'Serial Plotter', 
-      vscode.ViewColumn.One, 
-      {} 
+  let disposable = vscode.commands.registerCommand('serial-plotter.run', async () => {
+    const panel = vscode.window.createWebviewPanel(
+      'serialPlotter',
+      'Serial Plotter',
+      vscode.ViewColumn.One,
+      {        
+        enableScripts: true,
+      }
     );
-	});
 
-	context.subscriptions.push(disposable);
+    const filename = path.join(context.extensionPath, 'content', 'index.html');
+    panel.webview.html = await (await readFile(filename)).toString();
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
